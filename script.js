dedab6417deb595a9b1556ea360b420eeb74e1ca
unknown <129291511+dedsocks@users.noreply.github.com>
2025-08-09 19:38:45 +0530

    Change grid children color on hover

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let inputLabel = document.querySelector("#inputLabel");
 let inputColumns = document.querySelector("#inputColumns");
 let numberOfColumns = inputColumns.value;
 
+const blackBtn = document.querySelector("#black");
+const rainbowBtn = document.querySelector("#rainbow");
+let colorOnHover = "black";
 
 addGridSquares();
 
@@ -22,14 +25,27 @@ function addGridSquares(){
     for(let i = 0; i < numberOfSquares; i++){
         gridSquare[i] = document.createElement("div");
 
-        gridSquare[i].style.width = `${(gridContainerSideLength/numberOfColumns)-0.01}px`;//The 0.01 ensures that the dom elements will be slightly smaller than the parent element so that they can flex and fill up the rest of the area instead of wrapping due to availability of less space because of higher children side values due to css rem calculations.
-        
+        gridSquare[i].style.width = `${(gridContainerSideLength/numberOfColumns)-0.01}px`;//The 0.01 ensures that the child elements will be slightly smaller than the parent element so that they can flex and fill up the rest of the area instead of wrapping due to availability of less space because of inflated values on calc of gridContainerSideLength/numberOfColumns due to floating point precision errors.
+
         gridSquare[i].style.flex = "1 0 auto"
         gridSquare[i].style.height = `auto`;
         gridSquare[i].style.aspectRatio = "1/1";
-        
+
         gridSquare[i].classList.add("gridChild");
         gridSquare[i].classList.toggle("defaultGridChildColor"); 
+
+
+        gridSquare[i].addEventListener("mouseover",()=>{
+            switch(colorOnHover){
+                case "black":
+                    gridSquare[i].style.backgroundColor = `rgba(0,0,0,1)`;
+                    break;
+                case "rainbow":
+                    gridSquare[i].style.backgroundColor = `rgba(255,${Math.random()*255},${Math.random()*255},1)`;
+                    break;
+            }
+        })
+
         gridContainer.appendChild(gridSquare[i]);
     }
 }
@@ -39,3 +55,5 @@ function removeExistingGridSquares(){
     gridContainer.innerHTML = '';
 }
 
+blackBtn.addEventListener("click",()=>{colorOnHover = "black";})
+rainbowBtn.addEventListener("click",()=>{colorOnHover = "rainbow";})
